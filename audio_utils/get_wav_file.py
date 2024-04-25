70bf10162b7541ba808fc43787cd56884102e9a2
Amartya Varma <amartya.varma@gmail.com>
2024-04-25 02:46:02 +0100

    F

--- audio_utils/get_wav_file.py
+++ audio_utils/get_wav_file.py
@@ -2,11 +2,7 @@ import requests
 import base64
 from audio_utils.resample_audio import resample_audio
 
-api_url = 'http://34.141.243.146:8080/api/v1/static'
-ping_url = 'http://34.141.243.146:8080/ping'
-
-
-    # Parameters to send to the API
+api_url = 'http://35.234.101.2:8080:8080/api/v1/static'
 
 
 def get_wav_file(text):
