7fa747805882a187b54d9461ba4a3dd21f81dca8
Amartya Varma <amartya.varma@gmail.com>
2024-04-25 02:59:53 +0100

    F

--- audio_utils/get_wav_file.py
+++ audio_utils/get_wav_file.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 from audio_utils.resample_audio import resample_audio
+import base64
 
 api_url = 'http://35.234.101.2:8080/api/v1/static'
 
@@ -22,6 +23,13 @@ def get_wav_file(text):
 
     response = requests.post(api_url, json=data)
     audio_base64 = response.json()['audio_base64']
+
+    # Decode the base64 string to audio data
+    audio_data = base64.b64decode(audio_base64)
+
+    # Write the audio data to a WAV file
+    with open('speech.wav', 'wb') as file:
+        file.write(audio_data)
 
     return audio_base64
 
