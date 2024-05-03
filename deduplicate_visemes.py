cee3a7a9fe78cc1f252766db08418f40694e5d1f
Amartya Varma <amartya.varma@gmail.com>
2024-05-04 00:24:39 +0100

    f

--- deduplicate_visemes.py
+++ deduplicate_visemes.py
@@ -1,16 +1,12 @@
 def deduplicate_visemes(data):
-    # List to hold indices where modifications should be applied
+
+    print("depluicate_data: ", data)
     indices_to_modify = []
 
     # Identify all indices that need modification
     for i in range(len(data) - 1):
         if data[i]['targets'] == data[i + 1]['targets']:
             indices_to_modify.append(i)
-
-            print(data[i]['targets'])
-            print(data[i + 1]['targets'])
-            print("***")
-            
 
     # Apply modifications at the identified indices
     for i in indices_to_modify:
