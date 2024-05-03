e5f027dd151011b864224436a642d1b79fe4eddf
Amartya Varma <amartya.varma@gmail.com>
2024-05-04 00:29:34 +0100

    f

--- deduplicate_visemes.py
+++ deduplicate_visemes.py
@@ -1,19 +1,18 @@
 def deduplicate_visemes(data):
-
-    print("depluicate_data: ", data)
+    print("duplicate_data: ", data)
     indices_to_modify = []
 
-    # Identify all indices that need modification
+    # Check for equality between elements 0 through 32 inclusive
     for i in range(len(data) - 1):
-        if data[i]['targets'] == data[i + 1]['targets']:
+        if data[i]['targets'][:33] == data[i + 1]['targets'][:33]:
             indices_to_modify.append(i)
 
     # Apply modifications at the identified indices
     for i in indices_to_modify:
-        data[i]['targets'] = [x * 0.8 for x in data[i]['targets']]
-        data[i]['targets'][0] += 0.2  # Add 0.2 to the first element
+        # Only scale elements 0 through 32
+        scaled_targets = [x * 0.8 for x in data[i]['targets'][:33]] + data[i]['targets'][33:]
+        scaled_targets[0] += 0.2  # Add 0.2 to the first element of the scaled portion
+        data[i]['targets'] = scaled_targets
 
     return data
 
-
-    
