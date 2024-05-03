e213e90a0e259d2dfab798af58052ad33f1582f1
Amartya Varma <amartya.varma@gmail.com>
2024-05-04 00:42:01 +0100

    f

--- implementRR.py
+++ implementRR.py
@@ -1,6 +1,6 @@
 def implementRR(word_data):
     for entry in word_data:
-        if entry['graphemes'][0] == 'R' and len(entry['graphemes']) > 1 and entry['graphemes'][1] == 'R':
+        if entry['graphemes'][0] == 'R':
             entry['graphemes'][0] = 'RR'
             del entry['graphemes'][1]
 
