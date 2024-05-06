eababc94f7d53f59695a77d56e2604ffb107f701
Amartya Varma <amartya.varma@gmail.com>
2024-05-06 15:00:56 +0100

    f

--- animate/remove_tiny_durations.py
+++ animate/remove_tiny_durations.py
@@ -2,6 +2,10 @@ from animate.check_for_tiny_durations import check_for_tiny_durations
 from animate.remove_shortest_duration import remove_shortest_durations
 
 def remove_tiny_durations(nested_list):
+    print("Removing tiny durations")
+    print(nested_list)
+    print("Checking for tiny durations")
+    print(check_for_tiny_durations(nested_list))
     while check_for_tiny_durations(nested_list):
         nested_list = remove_shortest_durations(nested_list)
     return nested_list
