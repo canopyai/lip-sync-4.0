1569e55eacccfe22653cc7e3c909e7ff4fdce1f8
Amartya Varma <amartya.varma@gmail.com>
2024-05-10 13:07:38 +0100

    f

--- head_movements/add_eyebrow_movements.py
+++ head_movements/add_eyebrow_movements.py
@@ -23,8 +23,6 @@ def add_eyebrow_movements(int_alvs, emotion):
                 alv['targets'].extend([0, 0, 0, 0])
 
 
-        for _ in range(4):
-            alv['targets'].append(random.uniform(0, 1))
 
         print(f'Start index: {startIndex}, End index: {endIndex}, Strength: {strength}')
     else:
@@ -32,5 +30,7 @@ def add_eyebrow_movements(int_alvs, emotion):
         print("Not enough data to perform eyebrow movements, extending with zeros.")
         for alv in int_alvs:
             alv['targets'].extend([0, 0, 0, 0])
+    for _ in range(4):
+        alv['targets'].append(random.uniform(0, 1))
 
     return int_alvs
