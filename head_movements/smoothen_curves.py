9b87d1491c3761b83a78e0a1f2640da1e39bdba6
Amartya Varma <amartya.varma@gmail.com>
2024-05-11 16:44:25 +0100

    f

--- head_movements/smoothen_curves.py
+++ head_movements/smoothen_curves.py
@@ -4,7 +4,7 @@ def ease_in_out(t):
     """Cubic ease-in-ease-out function."""
     return 3 * t**2 - 2 * t**3
 
-def smoothen_curves(animations, step_duration=15, decay_steps=10):
+def smoothen_curves(animations, step_duration=15):
     new_animations = []
     for i in range(len(animations) - 1):
         initial_targets = animations[i]['targets']
@@ -17,12 +17,6 @@ def smoothen_curves(animations, step_duration=15, decay_steps=10):
             t = step / steps
             eased_t = ease_in_out(t)
             interpolated_targets = [initial + (final - initial) * eased_t for initial, final in zip(initial_targets, final_targets)]
-
-            # Apply decay to the last 10 elements of each sequence
-            if step >= steps - decay_steps + 1:
-                decay_factor = (steps - step) / decay_steps
-                interpolated_targets = [target * decay_factor for target in interpolated_targets]
-
             new_animations.append({'duration': step_duration, 'targets': interpolated_targets})
 
     return new_animations
