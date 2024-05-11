a437e04ede742b7f408e1099b11913ffd73fdfce
Amartya Varma <amartya.varma@gmail.com>
2024-05-11 16:54:19 +0100

    f

--- head_movements/smoothen_curves.py
+++ head_movements/smoothen_curves.py
@@ -5,16 +5,6 @@ def ease_in_out(t):
     return 3 * t**2 - 2 * t**3
 
 
-def generate_half_sine_wave(steps):
-
-    # Create an array of angles from pi to 2*pi
-    angles = np.linspace(0,np.pi,steps)
-    
-    # Compute the sine of these angles
-    sine_values = np.sin(angles)
-    
-    return sine_values.tolist()
-
 
 def smoothen_curves(animations, step_duration=15):
     new_animations = []
@@ -23,21 +13,12 @@ def smoothen_curves(animations, step_duration=15):
         final_targets = animations[i + 1]['targets']
         total_duration = animations[i]['duration']
         steps = total_duration // step_duration
-        sine_steps = generate_half_sine_wave(steps+1)
 
         # Calculate the interpolated targets for each step
-        
         for step in range(steps + 1):
             t = step / steps
             eased_t = ease_in_out(t)
             interpolated_targets = [initial + (final - initial) * eased_t for initial, final in zip(initial_targets, final_targets)]
             new_animations.append({'duration': step_duration, 'targets': interpolated_targets})
-    
-
-        for i in range(len(new_animations)):
-            new_animations[i]['targets'] = [x * sine_steps[i] for x in new_animations[i]['targets']]
-    
-
-
 
     return new_animations
