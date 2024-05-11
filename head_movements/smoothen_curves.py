98876d902799f9db5f066ad6f67c50ac99ea4c39
Amartya Varma <amartya.varma@gmail.com>
2024-05-11 16:53:07 +0100

    f

--- head_movements/smoothen_curves.py
+++ head_movements/smoothen_curves.py
@@ -23,6 +23,7 @@ def smoothen_curves(animations, step_duration=15):
         final_targets = animations[i + 1]['targets']
         total_duration = animations[i]['duration']
         steps = total_duration // step_duration
+        sine_steps = generate_half_sine_wave(steps+1)
 
         # Calculate the interpolated targets for each step
         
@@ -31,10 +32,10 @@ def smoothen_curves(animations, step_duration=15):
             eased_t = ease_in_out(t)
             interpolated_targets = [initial + (final - initial) * eased_t for initial, final in zip(initial_targets, final_targets)]
             new_animations.append({'duration': step_duration, 'targets': interpolated_targets})
-    sine_steps = generate_half_sine_wave(steps+1)
+    
 
-    for i in range(len(new_animations)):
-        new_animations[i]['targets'] = [x * sine_steps[i] for x in new_animations[i]['targets']]
+        for i in range(len(new_animations)):
+            new_animations[i]['targets'] = [x * sine_steps[i] for x in new_animations[i]['targets']]
     
 
 
