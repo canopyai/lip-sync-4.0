fd34f6876505480ebb5df4d415db173afd8429b1
Amartya Varma <amartya.varma@gmail.com>
2024-05-13 17:16:08 +0100

    f

--- head_movements/orchestrate_head_movement_curves.py
+++ head_movements/orchestrate_head_movement_curves.py
@@ -9,7 +9,7 @@ from head_movements.interpolate_with_easing import interpolate_with_cumulative_e
 def orchestrate_head_movement_curves(segments):
     mov_tups = compute_head_movement_tuples(segments)
     all_visemes = convert_tuples_to_curves(mov_tups)
-    alvs = interpolate_with_cumulative_easing(all_visemes) 
+    # alvs = interpolate_with_cumulative_easing(all_visemes) 
     alvs = add_durations(all_visemes)
     int_alvs = integrate_head_movements(alvs)
     # int_alvs = add_eyebrow_movements(int_alvs, "happy")
