2d230805909fbe57308fcc1a03d7bc206d53da75
Amartya Varma <amartya.varma@gmail.com>
2024-05-13 21:55:49 +0100

    f

--- head_movements/orchestrate_head_movement_curves.py
+++ head_movements/orchestrate_head_movement_curves.py
@@ -3,8 +3,8 @@ from head_movements.convert_tuples_to_curves import convert_tuples_to_curves
 from head_movements.add_durations import add_durations
 from head_movements.integrate_head_movements import integrate_head_movements
 from head_movements.interpolate_with_easing import interpolate_with_cumulative_easing
-# from head_movements.calculate_eye_stabilisations import calculate_eye_stabilisations
 from head_movements.add_eyebrow_movements import add_eyebrow_movements
+# from head_movements.calculate_eye_stabilisations import calculate_eye_stabilisations
 
 
 def orchestrate_head_movement_curves(segments):
@@ -12,8 +12,10 @@ def orchestrate_head_movement_curves(segments):
     all_visemes = convert_tuples_to_curves(mov_tups)
     all_visemes_copy = all_visemes.copy()
     alvs = interpolate_with_cumulative_easing(all_visemes) 
-    alvs_copy = add_durations(all_visemes_copy)
+    alvs = add_durations(all_visemes)
     int_alvs = integrate_head_movements(alvs)
-    int_alvs_copy = int_alvs.copy()
-    int_alvs_brows = add_eyebrow_movements(int_alvs_copy, "happy")
-    return int_alvs, int_alvs_brows
+
+    alvs_copy = add_durations(all_visemes_copy)
+    int_alvs_copy = integrate_head_movements(alvs_copy)
+    int_alvs_copy = add_eyebrow_movements(int_alvs_copy, "happy")
+    return int_alvs, int_alvs_copy
