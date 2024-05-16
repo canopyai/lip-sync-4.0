45e0159a6e6421d211c762e1bfeccbbcbda4080e
Amartya Varma <amartya.varma@gmail.com>
2024-05-16 20:06:24 +0100

    f

--- head_movements/compute_head_movement_tuples.py
+++ head_movements/compute_head_movement_tuples.py
@@ -1,35 +1,41 @@
 import math
 import random
 
-
 def compute_head_movement_tuples(segments):
-  head_movement_tuples = []
-  first_segment_start = segments[0]['start']
-  first_segment_end = segments[0]['end']
+    head_movement_tuples = []
 
-  first_segment_start_tuple = (first_segment_start, 0.5)
-  second_segment_start_tuple = (first_segment_end, 0.25)
+    # Initial segments handling
+    first_segment_start = segments[0]['start']
+    first_segment_end = segments[0]['end']
 
-  head_movement_tuples.append(first_segment_start_tuple)
-  head_movement_tuples.append(second_segment_start_tuple)
+    first_segment_start_tuple = (first_segment_start, 0.5)
+    second_segment_start_tuple = (first_segment_end, 0.25)
 
-  index_of_stress = math.ceil(len(segments)/2)
+    head_movement_tuples.append(first_segment_start_tuple)
+    head_movement_tuples.append(second_segment_start_tuple)
 
-  mid_tuple = (segments[index_of_stress]["start"], 0.7)
-  head_movement_tuples.append(mid_tuple)
+    # Midpoint calculation
+    index_of_stress = math.ceil(len(segments) / 2)
 
-  remaining_indices = [i for i in range(len(segments))]
-  remaining_indices.remove(0)
-  remaining_indices.remove(1)
-  remaining_indices.remove(index_of_stress)
+    mid_tuple = (segments[index_of_stress]["start"], 0.7)
+    head_movement_tuples.append(mid_tuple)
 
-  selected_list = [index for index in remaining_indices if random.random() < 2/5]
+    # Create a list of remaining indices
+    remaining_indices = [i for i in range(len(segments))]
+    remaining_indices.remove(0)
+    remaining_indices.remove(1)
 
-  for i in selected_list:
-    sel = segments[i]
-    head_movement_tuples.append((sel["start"], random.uniform(0.1, 0.25)))
+    # Ensure index_of_stress is neither 0 nor 1 before removing
+    if index_of_stress in remaining_indices:
+        remaining_indices.remove(index_of_stress)
 
-  head_movement_tuples.sort(key=lambda x: x[0])
+    # Select remaining indices randomly
+    selected_list = [index for index in remaining_indices if random.random() < 2 / 5]
 
+    for i in selected_list:
+        sel = segments[i]
+        head_movement_tuples.append((sel["start"], random.uniform(0.1, 0.25)))
 
-  return head_movement_tuples
+    head_movement_tuples.sort(key=lambda x: x[0])
+
+    return head_movement_tuples
