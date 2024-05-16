01658a30c83e73c06f1cfb89d558b572d02599c4
Amartya Varma <amartya.varma@gmail.com>
2024-05-16 01:46:37 +0100

    f

--- head_movements/get_eyebrow_selection_movement.py
+++ head_movements/get_eyebrow_selection_movement.py
@@ -1,17 +1,17 @@
 def get_eyebrow_selection_movement(emotion):
   if (emotion == "happy"):
-    return [0,0.8,0,0.8]
+    return [0.8,0.8,0,0]
   elif (emotion == "neutral"):
     return [0.7,0,0,0]
   elif (emotion == "sad"):
-    return [1,0,0,0]
+    return [0,0,1,0]
   elif (emotion == "fear"):
-    return [1,0,0,0]
+    return [0,0,0.5,0.5]
   elif (emotion == "excitement"):
     return [1,0,0,0]
   elif (emotion == "disgust"):
-    return [1,0,0,0]
+    return [0,0.3,0,0.7]
   elif (emotion == "concerned"):
-    return [1,0,0,0]
+    return [0,0,0.8,0.3]
   elif (emotion == "angry"):
-    return [0,0,0.8,0]
+    return [0,0,0.8,0.3]
