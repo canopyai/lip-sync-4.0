ee05a0dd64913e949c73241cee2dde7cae38b32c
Amartya Varma <amartya.varma@gmail.com>
2024-05-17 01:37:54 +0100

    f

--- head_movements/interpolate_with_easing.py
+++ head_movements/interpolate_with_easing.py
@@ -1,13 +1,25 @@
 import numpy as np
 
-
 def cubic_ease_in_out(t):
+    """
+    Cubic ease-in-out function for smooth transitions.
+    
+    :param t: Normalized time value (0 <= t <= 1).
+    :return: Eased time value.
+    """
     if t < 0.5:
         return 4 * t ** 3
     else:
         return 1 - 4 * (1 - t) ** 3
 
 def interpolate_with_cumulative_easing(data, interval_ms=15):
+    """
+    Interpolates movement data with cumulative cubic ease-in-out easing.
+    
+    :param data: List of dicts, each with 'start' and 'deltas' for movement transitions.
+    :param interval_ms: Time interval in milliseconds for each interpolation step.
+    :return: List of dicts with eased deltas and corresponding timestamps.
+    """
     result = []
     current_values = [0] * 6  # Start values for 6 targets
     
@@ -22,7 +34,8 @@ def interpolate_with_cumulative_easing(data, interval_ms=15):
             duration = 1000  # Default duration of 1000ms for the last segment
         
         num_frames = int(duration / interval_ms)
-        for i in range(num_frames + 1):
+        
+        for i in range(num_frames):
             t = i / num_frames
             eased_t = cubic_ease_in_out(t)
             
@@ -33,9 +46,7 @@ def interpolate_with_cumulative_easing(data, interval_ms=15):
                 'start': start_index + i * interval_ms
             })
         
-        # Update current_values to the last values of this segment
+        # Update current_values to the target values for the next segment
         current_values = [current_values[j] + deltas[j] for j in range(6)]
     
     return result
-
-# We can re-run this function on your data and plot the results.
