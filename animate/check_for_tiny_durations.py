d15815ee0cf404b9c270127616cbd1abcd17e237
Amartya Varma <amartya.varma@gmail.com>
2024-05-21 03:09:59 +0100

    f

--- animate/check_for_tiny_durations.py
+++ animate/check_for_tiny_durations.py
@@ -2,7 +2,7 @@ def check_for_tiny_durations(list):
     # Iterate through each element in the list
     for item in list[0]:
         # Check if the duration of the current element is less than 0.03
-        if item['duration'] < 40:
+        if item['duration'] < 50:
             # If found, return True
             return True
     # If the loop completes without finding any duration less than 0.03, return False
